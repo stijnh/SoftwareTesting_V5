0ef4f27fe077eec2ce23a014b09b9fbdb992bfbb
Jimi van der Woning <jimivdwoning@gmail.com>
2013-09-03 09:32:16 +0200

    Prettified and refactored code a bit

--- week1/jimi/GS.hs
+++ week1/jimi/GS.hs
@@ -2,63 +2,61 @@ module GS where
 
 -- Exercise 1.9
 mxmInt :: [Int] -> Int
-mxmInt []     = error "empty list"
-mxmInt [x]    = x
-mxmInt (x:xs) = max x (mxmInt xs)
+mxmInt [] = error "empty list"
+mxmInt [x] = x
+mxmInt (x : xs) = max x (mxmInt xs)
 
 -- Exercise 1.10
 removeFst :: Eq a => a -> [a] -> [a]
-removeFst m []     = error "empty list"
-removeFst m [x]    | x == m = []
-                   | otherwise = [x]
-removeFst m (x:xs) | x == m = xs
-                   | otherwise = x : (removeFst m xs)
+removeFst m [] = error "empty list"
+removeFst m [x]
+  | x == m = []
+  | otherwise = [x]
+removeFst m (x : xs)
+  | x == m = xs
+  | otherwise = x : (removeFst m xs)
 
 -- Exercise 1.13
 count :: Char -> String -> Int
-count c []     = 0
-count c [x]    | c == x = 1
-               | otherwise = 0
-count c (x:xs) | c == x = 1 + (count c xs)
-               | otherwise = (count c xs)
+count c [] = 0
+count c (x : xs)
+  | c == x = 1 + (count c xs)
+  | otherwise = (count c xs)
 
 -- Exercise 1.14
-blowup :: String -> String
-blowup s = let
-             rptChar :: Char -> Int -> String
-             rptChar c n | n < 0 = error "n should be a positive integer"
-                         | n == 0 = []
-                         | otherwise = c : (rptChar c (n-1))
+rptChar :: Char -> Int -> String
+rptChar c n
+  | n < 0 = error "n should be a positive integer"
+  | n == 0 = []
+  | otherwise = c : (rptChar c (n - 1))
 
-             blwpHlpr :: String -> Int -> String
-             blwpHlpr [] n = []
-             blwpHlpr [x] n = rptChar x n
-             blwpHlpr (x:xs) n = (rptChar x n) ++ (blwpHlpr xs (n+1))
-           in
-             blwpHlpr s 1
+blwpHlpr :: String -> Int -> String
+blwpHlpr [] n = []
+blwpHlpr (x : xs) n = (rptChar x n) ++ (blwpHlpr xs (n + 1))
+
+blowup :: String -> String
+blowup s = blwpHlpr s 1
 
 -- Exercise 1.15
+mnmStr :: [String] -> String
+mnmStr [] = error "empty list"
+mnmStr [x] = x
+mnmStr (x : xs) = min x (mnmStr xs)
+
 srtString :: [String] -> [String]
 srtString [] = []
 srtString [x] = [x]
-srtString xs = let
-                 mnmStr :: [String] -> String
-                 mnmStr [] = error "empty list"
-                 mnmStr [x] = x
-                 mnmStr (x:xs) = min x (mnmStr xs)
-               in
-                 (mnmStr xs) : srtString (removeFst (mnmStr xs) xs)
+srtString xs = (mnmStr xs) : srtString (removeFst (mnmStr xs) xs)
 
 -- Exercise 1.17
 prefix :: String -> String -> Bool
 prefix [] ys = True
-prefix (x:xs) [] = False
-prefix (x:xs) (y:ys) = (x == y) && (prefix xs ys)
+prefix (x : xs) [] = False
+prefix (x : xs) (y : ys) = (x == y) && (prefix xs ys)
 
 substring :: String -> String -> Bool
-substring [] ys = True
-substring (x:xs) [] = False
-substring xs (y:ys) = (prefix xs (y:ys)) || (substring xs ys)
+substring (x : xs) [] = False
+substring xs (y : ys) = (prefix xs (y : ys)) || (substring xs ys)
 
 -- Exercise 1.20
 lengths :: [[a]] -> [Int]
@@ -67,6 +65,4 @@ lengths xs = map length xs
 -- Exercise 1.21
 sumLengths :: [[a]] -> Int
 sumLengths xs = sum (lengths xs)
-
-
 
